2d3bd1a77ae92fe4dca663ea0b7e04a3f2f9c58f
micmarsh <michael.marsh42@gmail.com>
2013-02-16 14:33:17 -0500

    Removed nostack and recursive modes due to excessive ugliness

--- src/generator/core.clj
+++ src/generator/core.clj
@@ -30,30 +30,6 @@
                     :fifth [:shit " yeah "]
                     :sixth [:first "! " :second "third" :fourth :fifth :second :second]
                     })
-;da rules: given a :key:
-; map [vec...] (fn[item]( (if list? item) (rand-nth item) ()  ))
-
-;preserve this for benchmarks
-(defn follow-keyword [k, grammar]
-    (let [value (grammar k)
-          proc-vector (fn[v, grammar](map
-            (fn[item](cond
-                (keyword? item)
-                        (follow-keyword item grammar)
-                (string? item)
-                        (str item))) v) )]
-        (cond
-            (vector? value)
-                (apply str (proc-vector value grammar))
-            (list? value)
-                (rand-nth value))))
-;*for "replacing" shit, having a reduction with (apply conj [accumulator] [vec to conj])
-; somewhere in there would be the shit
-;loop through the whole thing, replacing keywords with either their list or
-;the things in their vector AND evaling list AT THE SAME TIME:
-; [:greeting " " ...] -> [:greeting-word :middle :world " " ...]
-;-> [(list ....) (list ....) :word :punc " " ...] -> ["Goodbye" ", " "Brother" (list ... ) (list ...) " " ...]
-;when everything's a string, kill loop and (apply str [...])
 
 (defn- eval-item [acc, item]
     (cond
@@ -92,35 +68,5 @@
             (apply str sequence)
             (recur (single-vector-passthrough sequence grammar)))))
 
-(defn no-stack-eval [grammar]
-    (loop [sequence (:main grammar)]
-        (if (reduce #(and (string? %2) %1)
-                true sequence)
-            (apply str sequence)
-            (recur  (reduce (fn[acc, item]
-                    (cond
-                        (keyword? item)
-                            (let [value (grammar item)]
-                                (cond
-                                    (vector? value)
-                                        (apply conj acc value)
-                                    (list? value)
-                                        (conj acc value)))
-                        (list? item)
-                            (conj acc (rand-nth item))
-                        (string? item)
-                            (conj acc item)))
-                [] sequence )))))
-
-
 (defn -main [& args]
-    (let [argv (vec args)]
-        (cond
-            (< (count argv) 1)
-                (println "You didn't specify a type: 'recur', 'loop', or 'nostack'")
-            (= (argv 0) "recur")
-                (follow-keyword :main example)
-            (= (argv 0) "loop")
-                (eval-grammar example)
-            (= (argv 0) "nostack")
-                (no-stack-eval example))))
+    (eval-grammar example))
