b48c40c11e63e82ba75a59c79428040cb01c2b6b
micmarsh <michael.marsh42@gmail.com>
2013-02-16 19:18:38 -0500

    Basic themes tests, extremely difficult to test anything random at this point

--- test/generator/core_test.clj
+++ test/generator/core_test.clj
@@ -1,7 +1,28 @@
 (ns generator.core-test
   (:use clojure.test
-        generator.core))
+        generator.core
+        generator.parser))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest themes-1
+    (testing "themes!"
+        (is (=
+            (eval-grammar
+                {
+                :themes [:foo]
+                :main [{:foo "bar" :not-foo "baz"}]
+                })
+            "bar"))))
+
+(deftest themes-2
+    (testing "themes!"
+        (is (=
+            (eval-grammar
+                {
+                :themes [:foo :bar]
+                :main [{:foo "bar" :not-foo "baz"} " crawl " {:bar "tonight" :not-bar "zex"}]
+                })
+            "bar crawl tonight"))))
