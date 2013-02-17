557b61bcc4e925173bfc8c66b1b9ff6868b7f726
micmarsh <michael.marsh42@gmail.com>
2013-02-16 19:05:33 -0500

    Themes base case test is smashing success

--- src/generator/core.clj
+++ src/generator/core.clj
@@ -4,25 +4,25 @@
 ; reserved keywords: :main, :themes
 
 (def example {
-            :main [:greeting  " " :second-phrase]
-                :greeting [:greeting-word :middle :world]
-                    :greeting-word (list "Hello" "Goodbye")
-                    :middle (list " " ", ")
-                    :world [ :word :punc ]
-                        :word (list "World" "Mother" "Brother" "earthlings")
-                        :punc (list "!" "." "?")
-                :second-phrase [:emotion " to meet you, " :end-phrase :punc]
-                    :emotion (list "Pleased" "Inconvenienced" "Aroused")
-                    :end-phrase (list "hope you guess my name"
-                                      "I need to go sleep now")
-                        })
+        :main [:greeting  " " :second-phrase]
+            :greeting [:greeting-word :middle :world]
+                :greeting-word (list "Hello" "Goodbye")
+                :middle (list " " ", ")
+                :world [ :word :punc ]
+                    :word (list "World" "Mother" "Brother" "earthlings")
+                    :punc (list "!" "." "?")
+            :second-phrase [:emotion " to meet you, " :end-phrase :punc]
+                :emotion (list "Pleased" "Inconvenienced" "Aroused")
+                :end-phrase (list "hope you guess my name"
+                                  "I need to go sleep now")
+    })
 
 (def theme-test {
-        :themes [:funny :romantic]
+        :themes [:funny :sad]
         :main [{:funny "hahahaha" :sad "boohoohoo"} " " :end]
-            :end {}
+            :end {:funny "lulz lulz lulz" :romantic "I love you!"}
     })
 
 
 (defn -main [& args]
-    (eval-grammar example))
+    (eval-grammar theme-test))
