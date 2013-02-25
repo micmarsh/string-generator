226115c0f7ebc1b349eab44cb059987e3be42d0b
micmarsh <michael.marsh42@gmail.com>
2013-02-24 20:12:16 -0500

    Created introduction to political speech

--- src/generator/core.clj
+++ src/generator/core.clj
@@ -33,7 +33,7 @@
 
 ;possible themes: funny, sad, romantic
 (def letter {
-        :themes [:funny]
+        :themes [ (list :sad :romantic :funny) ]
         :main [:salutation "\n\n" :paragraph "\n\n" :signature]
             :salutation[{:romantic "My Darling" :else "To Whom It May Concern"} ","]
             :paragraph [:opener " " :statement " " :closing]
@@ -75,6 +75,66 @@
 
     })
 
+;default theme combos: [ (list :mainstream :radical) (list :intellectual :frothing)]
+(def politcal-speech {
+        :themes [ (list :mainstream :radical) (list :intellectual :frothing)]
+        :main [:introduction ];:problem :take-action :closing]
+            :introduction [:address :follow-up :loaded-question]
+                :address ["My " {:intellectual "trusted" :frothing "outraged" :else "fellow"}
+                            {:intellectual "colleagues" :frothing "comrades"
+                            :radical "brothers and sisters in solidarity"
+                            :else "Americans"} ", " ]
+                :follow-up [ "6 years ago today," :enemy :did-bad-thing "."]
+                    :enemy {
+                        :mainstream (list "my opponent" :other-party)
+                        :radical "our capitalist oppressors"
+                        :else :other-party
+                    }
+                        :other-party "the other party"
+                    :did-bad-thing [:cause "that " :effect ]
+                        :cause {
+                            :mainstream (list "signed a bill" "won a primary election" )
+                            :radical (list "issued an edict" "hatched a plot")
+                            :else "did something"
+                        }
+                        :effect {
+                            :intellectual "changed the course of history"
+                            :frothing ["destroyed" (list "your wages" "your future")]
+                            :else "changed everything"
+                        }
+                :loaded-question [ (list ["Did you know " :upsetting-fact]
+                                    ["How long will you" :be-passive]) "?"]
+                    :upsetting-fact {
+                        :mainstream {
+                            :intellectual ["this nation "
+                                        "exports fewer horse-drawn carraiges now than"
+                                        (list "at any point in history" "in 1969")]
+                            :frothing ["the " (list "greedy" "fat-cat")" 1%"
+                                        (list "has more money than you"
+                                            "made record profits last year")]
+                        }
+                        :radical ["our" :unfortunate-circumstance
+                                    "is a direct result of" :evil-institution]
+                        :else "this"
+
+                    }
+                        :unfortunate-circumstance (list [(list "crippling" "degrading")
+                                                        (list "unemployment" "oppression")]
+                                                    "having to expend effort to survive")
+                        :evil-institution (list "the Patriarchy" "modern civilization" "rich people")
+                    :be-passive {
+                        :mainstream "continue to vote against the middle class"
+                        :intellectual "deny the neccesity of unfettered Keynesianism"
+                        :radical (list [ "continue to be robbed of your"
+                                    (list "freedoms" "dignity" "marijuana")]
+                                    "stand by in blissful ignorance with the rest of the sheeple")
+                    }
+
+
+
+
+    })
+
 
 (defn -main [& args]
-   (println (eval-grammar letter) ))
+   (println (eval-grammar politcal-speech) ))
