09883e43645d7b2a79ec4be90f649df1e76f23ec
micmarsh <mike@marsh.pw>
2014-08-02 16:39:09 -0400

    use test to check timing

--- test/generator/core_test.clj
+++ test/generator/core_test.clj
@@ -1,24 +1,10 @@
 (ns generator.core-test
   (:use clojure.test
-        generator.core
-        generator.parser))
+        [generator.example :only (political-speech)]
+        [generator.parser :only (eval-grammar)]))
 
-(deftest themes-1
-    (testing "themes!"
-        (is (=
-            (eval-grammar
-                {
-                :themes [:foo]
-                :main [{:foo "bar" :not-foo "baz"}]
-                })
-            "bar"))))
-
-(deftest themes-2
-    (testing "themes!"
-        (is (=
-            (eval-grammar
-                {
-                :themes [:foo :bar]
-                :main [{:foo "bar" :not-foo "baz"} " crawl " {:bar "tonight" :not-bar "zex"}]
-                })
-            "bar crawl tonight"))))
+(deftest timing
+  (let [theme (rand-nth [:mainstream :radical])
+        result (time (eval-grammar political-speech [theme]))]
+    (println "Using theme" (name theme))
+    #_(println "Result" result)))
