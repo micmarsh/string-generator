2a1e4c63a9256520fabe126ead2132e91cc89fc1
micmarsh <mike@marsh.pw>
2014-08-02 15:50:53 -0400

    update clojure!

--- project.clj
+++ project.clj
@@ -3,6 +3,38 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [marshmacros "0.1.1"]]
-  :main generator.core)
+  :main generator.core
+
+  :profiles {
+    :dev {
+        :dependencies [[org.clojure/clojurescript "0.0-2268"]]
+
+        :plugins [[com.keminglabs/cljx "0.4.0"]
+                  [lein-cljsbuild "1.0.3"]]
+
+        :hooks [cljx.hooks]
+
+        :cljsbuild {
+          :builds [{
+              :source-paths ["target/cljs"]
+              :compiler {
+                :output-to "target/main.js"
+                :optimizations :whitespace
+                :pretty-print true}}
+        ]}
+
+        :cljx {
+          :builds [
+                {:source-paths ["src/generator/"]
+                 :output-path "target/classes/generator"
+                 :rules :clj}
+                {:source-paths ["src/generator/"]
+                 :output-path "target/cljs/generator"
+                 :rules :cljs}
+        ]}
+
+    }
+  })
+
