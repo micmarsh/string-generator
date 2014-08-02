2155c6082a19a3b2a51615017619d8e29520bceb
micmarsh <mike@marsh.pw>
2014-08-02 15:47:10 -0400

    don't need jvm options

--- project.clj
+++ project.clj
@@ -5,7 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [marshmacros "0.1.1"]]
-  :core.typed {:check [generator.parser]}
-  :javac-target "1.7"
-  :java-target "1.7"
   :main generator.core)
