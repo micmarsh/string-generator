b5f03d4d86cc6e81ffa2c0027812ec2de37de777
micmarsh <mike@marsh.pw>
2014-08-03 13:19:02 -0400

    get rid of crap

--- src/generator/macros.clj
+++ src/generator/macros.clj
@@ -1,34 +1,5 @@
 (ns generator.macros)
 
-(defn- template? [object]
-  (cond
-    (symbol? object)
-      (let [evaled (eval object)]
-          (and
-            (map? evaled)
-            (:main evaled)))
-    (map? object)
-      (:main object)
-    :else
-      false))
-
-(defn- assoc-template [final-map [key value]]
-  (if (template? value)
-    (let [to-map (eval value)
-          main (:main to-map)
-          value-without-main (dissoc to-map :main)
-          map-with-main (assoc final-map key main)]
-      (merge map-with-main value-without-main))
-    (assoc final-map key value)))
-
-(defn- make-template-map [pairs]
-  (loop [final-map { }
-         pairs (partition 2 pairs)]
-    (if (= 0 (count pairs))
-      final-map
-      (let [pair (first pairs)]
-        (recur (assoc-template final-map pair) (rest pairs))))))
-
 (defn- test-keywords [pairs]
   (let [seen (atom #{ })]
     (doseq [[key value] pairs]
@@ -36,7 +7,8 @@
         (throw (Exception. (str "Duplicate key error: " key)))
         (swap! seen conj key)))))
 
-(def tuples #(->> % (partition 2) (map vec)))
+(def ^:private tuples
+  #(->> % (partition 2) (map vec)))
 
 (defmacro deftemplate [name & args]
   (let [needs-main (odd? (count args))
